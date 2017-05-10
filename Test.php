4a494e4cf809a3f09276f7e854624bd868ec1f17
sankarnadendla <nadendlasankar5@gmail.com>
2017-05-10 19:33:52 -0400

    Add files via upload

---
+++ Test.php
@@ -0,0 +1,14 @@
+<?php
+
+class Test extends PHPUnit_Framework_TestCase
+{
+	public function testOnePlusOne() {
+		$this->assertEquals(1+1,2);
+  	}
+
+	public function testOneMinusOne() {
+		$this->assertEquals(1-1,0);
+  	}
+}
+
+?>
